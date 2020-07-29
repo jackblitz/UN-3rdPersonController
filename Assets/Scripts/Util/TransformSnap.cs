c82a8263871c9fe70b1daa6415ab34479ae131c6
lukerealwear <luke@realwear.com>
2020-07-28 21:44:11 -0700

    Fix: Camera Jitter
    The players animation would causes the camera for move plus and negative 0.03, this meant that when calcuating angles based on user position the angle was always different even when moving forward because of this slight change
    
    New Camera Reference is used which contains Transform snapping. This locks the transform from moving this small amount each time.

---
+++ Assets/Scripts/Util/TransformSnap.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Makes the Transform snap to the specified snap values.
+/// </summary>
+public class TransformSnap : MonoBehaviour
+{
+
+    // snap to this value
+    public float snap = 0.01f;
+    public Transform _transform;
+    private Vector3 _offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        _offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = GetSharedSnapPosition(_transform.position + _offset, snap);
+    }
+
+    /// <summary>
+    /// Accepts a position, and sets each axis-value of the position to be snapped according to the value of snap
+    /// </summary>
+    public Vector3 GetSharedSnapPosition(Vector3 originalPosition, float snap = 0.01f)
+    {
+        return new Vector3(
+                Mathf.Round(originalPosition.x / snap) * snap,
+                originalPosition.y,
+                originalPosition.z
+            // Mathf.Round(originalPosition.y / snap) * snap,
+            //Mathf.Round(originalPosition.z / snap) * snap
+            );
+    }
+
+    /// <summary>
+    /// Accepts a value, and snaps it according to the value of snap
+    /// </summary>
+    public static float GetSnapValue(float value, float snap = 0.01f)
+    {
+        return (!Mathf.Approximately(snap, 0f)) ? Mathf.RoundToInt(value / snap) * snap : value;
+    }
+}
